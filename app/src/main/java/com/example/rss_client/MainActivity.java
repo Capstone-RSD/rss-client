0a552869c7370e58f2babfcda114df900e3bd90a
tiwaojo <tiwaojo@gmail.com>
2022-12-30 06:53:34 -0500

    feat: added proto file to gradle build
    Signed-off-by: tiwaojo <tiwaojo@gmail.com>

--- app/src/main/java/com/example/rss_client/MainActivity.java
+++ app/src/main/java/com/example/rss_client/MainActivity.java
@@ -1,33 +1,40 @@
 package com.example.rss_client;
 
 import android.Manifest;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.util.Log;
-import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.Toast;
-
+import com.example.rss_client.Client.DamageLocationOrBuilder;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    // Resource: https://youtu.be/2eQC8NuaMkw
+    /**
+     * Resource: https://youtu.be/2eQC8NuaMkw
+     * https://youtu.be/lPfQN-Sfnjw?list=PLrnPJCHvNZuB_7nB5QD-4bNg6tpdEUImQ
+     */
     Button selectBtn, uploadBtn;
     ArrayList<Uri> imageList = new ArrayList<Uri>();
+
+    // Create a Cloud Storage reference from the app
+    StorageReference rssStorageRef = FirebaseStorage.getInstance().getReference("uploads");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,90 +43,120 @@ public class MainActivity extends AppCompatActivity {
 
         selectBtn = findViewById(R.id.imgSelect);
         uploadBtn = findViewById(R.id.imgUpload);
+         uploadBtn.setEnabled(imageList.isEmpty());// Enable upload button if the list isn't empty
 
-        ActivityResultLauncher<Intent> startIntentActivityResultLauncher = getIntentActivityResultLauncher();
-
-        selectBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) { // Have we granted permission
-                    Toast.makeText(MainActivity.this,"Permission has been granted",Toast.LENGTH_SHORT).show();}
-                else if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
-                    new AlertDialog.Builder(MainActivity.this).setTitle("Permission needed").setMessage("This mpermission is needed for RSS").setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
-                        }
-                    }).setNegativeButton("Cancal", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    }).create().show();
-                }
-                   else {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
-                }
-
-//            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);//, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//            intent.setType("image/*");
-//            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
-//            startIntentActivityResultLauncher.launch(intent);
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);//, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                intent.setType("image/*");
-//            intent.putExtra(Intent.ACTION_PICK, 1);
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                startIntentActivityResultLauncher.launch(Intent.createChooser(intent, "Select item(s)"));
-            }
-
-        });
-
-    }
-
-    @NonNull
-    private ActivityResultLauncher<Intent> getIntentActivityResultLauncher() {
         ActivityResultLauncher<Intent> startIntentActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             // Resource: https://youtu.be/Ke9PaRdMcgc
             @Override
             public void onActivityResult(ActivityResult result) {
                 if (result.getResultCode() == RESULT_OK) {
+                    Log.i("Result code", String.valueOf(result.getResultCode()));
                     assert result.getData() != null;
-                    assert (result.getData().getClipData() != null);
-                    int currImg = 0;
-                    int totalImgSelected = result.getData().getClipData().getItemCount();
-
+                    int currImg = 0, totalImgSelected = 0;
                     Uri imgUri;
 
-                    while (currImg <= totalImgSelected) {
+                    Uri uri = result.getData().getData();
+                    Log.i("Uri data", "onActivityResult: " + uri.toString());
+
+                    if ((result.getData().getClipData() != null)) {
+
+                        totalImgSelected = result.getData().getClipData().getItemCount();
+
+                        while (currImg < totalImgSelected) {
 //                        imgUri= result.getData().getData();
 
-                        imgUri = result.getData().getClipData().getItemAt(currImg).getUri();
+                            imgUri = result.getData().getClipData().getItemAt(currImg).getUri();
+//                        Log.i("Img name",result.getData().getClipData().getItemAt(currImg).getText().toString()) ;
+                            imageList.add(imgUri);
+                            currImg++;
+                            Log.i("Image Intent: ", imgUri.toString());
+                        }
+
+                    } else {
+                        // Only 1 item was selected
+                        imgUri = result.getData().getData();
                         imageList.add(imgUri);
-                        currImg++;
+                        totalImgSelected++;
                         Log.i("Image Intent: ", imgUri.toString());
                     }
-
+                    Toast.makeText(MainActivity.this, totalImgSelected + " image(s) has been selected", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
-        return startIntentActivityResultLauncher;
+
+        selectBtn.setOnClickListener(v -> {
+            imageList.clear(); // clears the list so it can be populated
+
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {// If permission is denied
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+//                return;
+            }
+
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);//, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            startIntentActivityResultLauncher.launch(intent);
+//            Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//            intent.setType("image/*");
+//            intent.putExtra(Intent.ACTION_PICK, 1);
+//            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+//            startIntentActivityResultLauncher.launch(Intent.createChooser(intent, "Select item(s)"));
+        });
+
+        uploadBtn.setOnClickListener(v -> {
+            uploadImg();
+        });
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==100){
-            if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);//, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                intent.setType("image/*");
-//            intent.putExtra(Intent.ACTION_PICK, 1);
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                getIntentActivityResultLauncher().launch(Intent.createChooser(intent, "Select item(s)"));
-            }else{
-                Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
-            }
+    // Get File Extension from Uri
+    //TODO: Requires research
+    private String getFileExt(Uri uri) {
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(getContentResolver().getType(uri));
+    }
+
+    private void uploadImg() {
+        assert !imageList.isEmpty();
+        for (Uri uri : imageList
+        ) {
+            Log.i("Uri", String.valueOf(getFileName(uri).hashCode()));
+
+            StorageReference storageReference = rssStorageRef.child(System.currentTimeMillis() + '.' + getFileExt(uri));
+
+            storageReference.putFile(uri).addOnSuccessListener(taskSnapshot -> {
+                Toast.makeText(this, "Upload Successful", Toast.LENGTH_SHORT).show();
+                Log.i("Firebase Storage", storageReference.getPath());
+                Log.i("Firebase Storage", "uploadImg: image has been uploaded to firebase");
+                // TODO: Impl progress bar
+            }).addOnFailureListener(e -> {
+                Toast.makeText(this, "Upload failed. Please try again later", Toast.LENGTH_SHORT).show();
+                Log.e("Firebase Storage", e.getMessage());
+            }).addOnProgressListener(snapshot -> {
+                double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                // TODO: Complete with progress bar
+            });
         }
+        // TODO: Hash image name instead of usign currentTimeMills()
+        // TODO: Check for duplicates using hashed name
+        // TODO: Impl protobuf to application
+        // TODO: create bucket depending on user email/Id (Use Email as ID)
+        // TODO: Assign full path to blobURL of proto schema attribute
+    }
+
+    private String getFileName(Uri uri) {
+//        Uri returnUri = returnIntent.getData();
+        Cursor returnCursor =
+                getContentResolver().query(uri, null, null, null, null);
+        /*
+         * Get the column indexes of the data in the Cursor,
+         * move to the first row in the Cursor, get the data,
+         * and display it.
+         */
+        if (returnCursor != null && returnCursor.moveToFirst()) {
+            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            return returnCursor.getString(nameIndex);
+        }
+        return null;
     }
 }
