ab472e85ced84e12a11ce96584688475ada746f4
tiwaojo <tiwaojo@gmail.com>
2023-02-17 19:08:04 -0500

    feat: added provider to the rss_models.dart and camera_widget.dart

--- lib/models/rss_models.dart
+++ lib/models/rss_models.dart
@@ -3,56 +3,64 @@ import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:rssclient/generated/rsd-dart-gen/rss_client.pbserver.dart';
 
-const String URL = "pkc-3w22w.us-central1.gcp.confluent.cloud:443";
-const String PATH = "/kafka/v3/clusters";
+class RSSClient extends ChangeNotifier {
+  static const String URL = "pkc-3w22w.us-central1.gcp.confluent.cloud:443";
+  static const String PATH = "/kafka/v3/clusters";
 
-final Client rssClient =
-    Client(id: 123456, email: "johndoe@email.com", name: "John Doe");
-const String TOPIC = "/rss_topic";
-const String CLUSTER_ID = "/lkc-d91ond";
-const int SCHEMA_ID = 100001;
-const String API_KEY =
-    "NzJOM1dWWFJLU1AzQUZTQTpvTkU2eWVyYkNSSStVR05XalIwVkhJSFNUQzJBbVp2NmlBRW5malp6Y0gvMWM3NHY3UDJnSVltd3hlRnJ3eFc4";
+  Client _rssClient = Client();
 
-const rssChannel = MethodChannel("rssChannel");
+  Client get rssClient => _rssClient;
 
-Future<Position> _getCurrentLocation() async {
-  bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    return Future.error(
-        "Location service has been disabled. Please enable for functionality");
+  set rssClient(Client value) {
+    _rssClient = value;
   }
 
-  LocationPermission locationPermission = await Geolocator.checkPermission();
-  if (locationPermission == LocationPermission.denied) {
-    locationPermission = await Geolocator.requestPermission();
-    if (locationPermission == LocationPermission.denied ||
-        locationPermission == LocationPermission.deniedForever) {
+  static const String TOPIC = "/rss_topic";
+  static const String CLUSTER_ID = "/lkc-d91ond";
+  static const int SCHEMA_ID = 100001;
+  static const String API_KEY =
+      "NzJOM1dWWFJLU1AzQUZTQTpvTkU2eWVyYkNSSStVR05XalIwVkhJSFNUQzJBbVp2NmlBRW5malp6Y0gvMWM3NHY3UDJnSVltd3hlRnJ3eFc4";
+
+  static const rssChannel = MethodChannel("rssChannel");
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
       return Future.error(
-          "Location permissions has been denied. Please enable for functionality");
+          "Location service has been disabled. Please enable for functionality");
     }
+
+    LocationPermission locationPermission = await Geolocator.checkPermission();
+    if (locationPermission == LocationPermission.denied) {
+      locationPermission = await Geolocator.requestPermission();
+      if (locationPermission == LocationPermission.denied ||
+          locationPermission == LocationPermission.deniedForever) {
+        return Future.error(
+            "Location permissions has been denied. Please enable for functionality");
+      }
+    }
+    return await Geolocator.getCurrentPosition();
   }
-  return await Geolocator.getCurrentPosition();
-}
 
-void _liveLocation() {
-  LocationSettings locationSettings = const LocationSettings(
-      accuracy: LocationAccuracy.high, distanceFilter: 0);
-  Geolocator.getPositionStream(locationSettings: locationSettings)
-      .listen((event) {
-    debugPrint("Longitude: ${event.longitude}");
-    debugPrint("Latitude: ${event.latitude}");
-  });
-}
+  void _liveLocation() {
+    LocationSettings locationSettings = const LocationSettings(
+        accuracy: LocationAccuracy.high, distanceFilter: 0);
+    Geolocator.getPositionStream(locationSettings: locationSettings)
+        .listen((event) {
+      debugPrint("Longitude: ${event.longitude}");
+      debugPrint("Latitude: ${event.latitude}");
+    });
+  }
 
-Future<void> uploadImage(Client rssClient) async {
-  // final file = File();
-  final firebasePath = '${rssClient.email}/rss';
-  // final storageRef = FirebaseStorage.instance.ref().child(firebasePath);
-  // storageRef.putFile(blob);
+  Future<void> uploadImage(Client rssClient) async {
+    // final file = File();
+    final firebasePath = '${rssClient.email}/rss';
+    // final storageRef = FirebaseStorage.instance.ref().child(firebasePath);
+    // storageRef.putFile(blob);
 
-  // Uploading to firebase
-  // UploadTask task;
-  // final snapshot = await task.whenComplete(() => null);
-  // final urlDownload = await
+    // Uploading to firebase
+    // UploadTask task;
+    // final snapshot = await task.whenComplete(() => null);
+    // final urlDownload = await
+  }
 }
